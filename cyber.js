337f5a1807f8ce5b0024201021c654cfbf31421a
Ste Iddon <steiddon@gmail.com>
2021-02-10 14:49:46 +0000

    Updated to numeric values
    functions & classes now all working

--- cyber.js
+++ cyber.js
@@ -1,70 +1,107 @@
 class Animal {
     constructor(name) {
         this.name = name;
-        this.hungry = true;
-        this.thirsty = true;
-        this.bored = true;
+        this.hunger = 50;
+        this.thirst = 50;
+        this.boredom = 50;
+        this.happiness = 50;
+        this.energy = 50;
     }
     eat(){
-        if (this.hungry === true) {
+        if (this.hunger > 5) {
         console.log(this.name, "is enjoying some food")
-        this.hungry = false;
-        this.bored = true;
+        this.hunger = this.hunger -5;
+        this.boredom = this.boredom -1;
+        this.happiness = this.happiness +5;
+        this.energy = this.energy +5;
         return this;
         } else {
             console.log(this.name, "is already full")
             return this;
     }}
     play(){
-        if (this.bored === true){
+        if (this.boredom > 4 & this.energy > 4){
         console.log(this.name, "is now having fun")
-        this.bored = false;
-        this.thirsty = true;
-        this.hungry = true;
+        this.boredom = this.boredom -5;
+        this.thirst = this.thirst +5;
+        this.hunger = this.hunger +5;
+        this.happiness = this.happiness +5;
+        this.energy = this.energy -5;
         return this;
         } else {
             console.log(this.name, "is tired from all the playing")
             return this;
     }}
     drink(){
-        if (this.thirsty === true){
+        if (this.thirst > 4){
         console.log(this.name, "has a big drink of water")
-        this.thirsty = false;
-        this.bored = true;
+        this.thirst = this.thirst -5;
+        this.boredom = this.boredom -1;
+        this.happiness = this.happiness = +5;
+        this.energy = this.energy = +1;
         return this;
     } else {
         console.log(this.name, "is quenched!")
         return this;
     }}
-    updateStatus(){ // invoke this function when pet has been left alone for too long
-        if (this.thirsty === false || this.bored === false || this.hungry === false){
-            this.thirsty = true, this.bored = true, this.hungry = true;
-            console.log("status' changed to true");
+    sleep(){
+        if (this.energy < 50){
+        console.log(this.name, "went to sleep")
+        this.energy = this.energy +5;
+        this.hunger = this.hunger +1;
+        this.thirst = this.thirst +1;
+        this.boredom = this.boredom +1;
+        this.happiness = this.happiness +1;
+        return this;
+        } else {
+            console.log(this.name, "has had enough rest")
             return this;
-        }
+    }}
+    checkStatus(){
+        console.log("NAME:", this.name, "Happiness:", this.happiness, "Energy:", this.energy, "Hunger:", this.hunger, "Thirst:", this.thirst, "Boredom:", this.boredom)
     }
 }
 class Dog extends Animal {
     constructor(name) {
         super(name)
-        this.hungry = true;
-        this.thirsty = true;
-        this.bored = true;
+        this.hunger = 50;
+        this.thirst = 50;
+        this.boredom = 50;
+        this.happiness = 50;
+        this.energy = 50;
     }
 }
 class Cat extends Animal {
     constructor(name) {
         super(name)
-        this.hungry = true;
-        this.thirsty = true;
-        this.bored = true;
+        this.hunger = 50;
+        this.thirst = 50;
+        this.boredom = 50;
+        this.happiness = 50;
+        this.energy = 50;
+    }
+}
+class Rabbit extends Animal {
+    constructor(name) {
+        super(name)
+        this.hunger = 50;
+        this.thirst = 50;
+        this.boredom = 50;
+        this.happiness = 50;
+        this.energy = 50;
     }
 }
 
 const Zola = new Dog("Zola");
 const Luna = new Cat("Luna");
-Zola.drink().play().eat().drink().eat().play().updateStatus().drink().play().eat().updateStatus().drink().updateStatus().drink();
-Luna.drink().play().eat().play().drink().play().eat().eat()
+const Hopper = new Rabbit("Hopper");
+Zola.eat().play().play().play()
+Luna.play().drink().eat().sleep().eat().eat().sleep().sleep().checkStatus()
+Hopper.drink().drink().drink().drink().drink().drink().drink().drink().drink().drink().drink().drink().drink().drink().drink()
+Zola.checkStatus(Zola);
+Luna.checkStatus();
+Hopper.checkStatus();
+
 
 
 
