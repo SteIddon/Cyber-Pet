085c6fbc1bf58678b30a22241d85cf160fce829a
Ste Iddon <steiddon@gmail.com>
2021-02-11 12:43:12 +0000

    Styling
    Added some styling and images

--- dom.js
+++ dom.js
@@ -11,41 +11,45 @@ let statusBar = document.getElementById("status");
 let petDiv = document.getElementById("pet");
 let petName = document.getElementById("petname");
 let P // on its own = let P = undefined
+let img = 0;
 
 newGame.addEventListener("click", () => {
     newGame.style.display = "none";
     selectDog.style.display = "block";
     selectCat.style.display = "block";
     selectRab.style.display = "block";
+    petName.style.display = "block";
+    petName.textContent = "Choose your pet";
     playPet.style.display = "none";
-    petName.style.display = "none";
+    petDiv.style.display = "none";
     getStatus.style.display = "none";
     feedPet.style.display = "none";
     drinkPet.style.display = "none";
     sleepPet.style.display = "none";
+    statusBar.textContent = "";
     statusBar.style.display = "none";
-    petDiv.style.display = "none";
-    petName.style.display = "none";
     setInterval(P.leftAlone(), 1000);
 })
 
 selectDog.addEventListener("click", () => {
+    img = 3;
     P = new Dog(prompt("What would you like to call your Dog?"));
     selectDog.style.display = "none";
     selectCat.style.display = "none";
     selectRab.style.display = "none";
     playPet.style.display = "block";
-    petName.style.display = "block";
     getStatus.style.display = "block";
     feedPet.style.display = "block";
     drinkPet.style.display = "block";
     sleepPet.style.display = "block";
     statusBar.style.display = "block";
     petDiv.style.display = "block";
-    petName.style.display = "block";
     petName.textContent =`Name : ${P.name}`;
+    newGame.style.display = "block";
+    petDiv.innerHTML = '<img src="pet' + img + '.png">';  
 })
 selectCat.addEventListener("click", () => {
+    img = 2;
     P = new Cat(prompt("What would you like to call your Cat?"));
     playPet.style.display = "block";
     selectDog.style.display = "none";
@@ -57,10 +61,12 @@ selectCat.addEventListener("click", () => {
     sleepPet.style.display = "block";
     statusBar.style.display = "block";
     petDiv.style.display = "block";
-    petName.style.display = "block";
     petName.textContent =`Name : ${P.name}`;
+    newGame.style.display = "block";
+    petDiv.innerHTML = '<img src="pet' + img + '.png">';  
 })
 selectRab.addEventListener("click", () => {
+    img = 1;
     P = new Rabbit(prompt("What would you like to call your Rabbit?"));
     playPet.style.display = "block";
     selectDog.style.display = "none";
@@ -72,8 +78,10 @@ selectRab.addEventListener("click", () => {
     sleepPet.style.display = "block";
     statusBar.style.display = "block";
     petDiv.style.display = "block";
-    petName.style.display = "block";
     petName.textContent =`Name : ${P.name}`;
+    newGame.style.display = "block";
+    petDiv.innerHTML = '<img src="pet' + img + '.png">';  
+
 })
 
 
